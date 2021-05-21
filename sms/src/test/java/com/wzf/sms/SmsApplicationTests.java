dbc4d5706e54e1972dcd614c6f8185727c5f1c65
wzf <80504767@qq.com>
2021-05-21 12:18:57 +0800

    1.编写发送短信验证码测试类

---
+++ sms/src/test/java/com/wzf/sms/SmsApplicationTests.java
@@ -0,0 +1,36 @@
+package com.wzf.sms;
+
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.teaopenapi.models.Config;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsApplicationTests {
+    com.aliyun.dysmsapi20170525.Client createClient(String accessKeyId, String accessKeySecret) throws Exception {
+        Config config = new Config()
+                // 您的AccessKey ID
+                .setAccessKeyId(accessKeyId)
+                // 您的AccessKey Secret
+                .setAccessKeySecret(accessKeySecret);
+        // 访问的域名
+        config.endpoint = "dysmsapi.aliyuncs.com";
+         return new com.aliyun.dysmsapi20170525.Client(config);
+    }
+
+    @Test
+    void contextLoads() throws Exception {
+            com.aliyun.dysmsapi20170525.Client client = createClient("LTAI5tE7KpiTY5VMM4ZvkGxj", "6gDBAuElu6RrKnDjpcrRDaLOy2x5rV");
+            SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                    .setPhoneNumbers("15279620069")
+                    //.setPhoneNumbers("18179951340")
+                    .setSignName("往忆")
+                    .setTemplateCode("SMS_215070823")
+                    .setTemplateParam("{\"code\":\"666666\"}");
+            // 复制代码运行请自行打印 API 的返回值
+            client.sendSms(sendSmsRequest);
+        }
+
+    }
+
+
